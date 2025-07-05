f884a5f2c4d582f64b01e365f65e5ebe824f1d93
Mahm0od_Fawzii <96267024+Mahmoud945101@users.noreply.github.com>
2025-07-05 11:05:30 +0300

    Update FawryInternshipChallenge.java

--- src/main/java/com/fawry/fawryinternshipchallenge/FawryInternshipChallenge.java
+++ src/main/java/com/fawry/fawryinternshipchallenge/FawryInternshipChallenge.java
@@ -8,11 +8,11 @@ import com.fawry.fawryinternshipchallenge.Cart;
 
 public class FawryInternshipChallenge {
     public static void main(String[] args) {
-        Customer customer = new Customer("Mahmoud", 100); // رصيد قليل
+        Customer customer = new Customer("Mahmoud", 100); 
 
         Product cheese = new Cheese("Cheese", 100, 5, 200, java.time.LocalDate.of(2025, 7, 15));
         Cart cart = new Cart();
-        cart.add(cheese, 2); // سعرهم 200
+        cart.add(cheese, 2); 
         cart.checkout(customer);
     }
 }
